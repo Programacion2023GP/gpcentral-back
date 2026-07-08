3acc6b59d120ade0b1c7f384c73d3183fba65d95
Nestor <nestorpuentesin@gmail.com>
2026-07-08 13:35:09 -0600

    se modifico el proceso de guardado de imagen, para que no elimine la anterior si no se modifica 1

--- app/Http/Controllers/DepartmentController.php
+++ app/Http/Controllers/DepartmentController.php
@@ -12,6 +12,7 @@ use App\Models\VW_User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 
 class DepartmentController extends BaseCrudController
