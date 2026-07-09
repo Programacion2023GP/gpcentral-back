abf269e5be12d29dc7fb9a5e391c1ec94833b482
Nestor <nestorpuentesin@gmail.com>
2026-07-09 10:01:37 -0600

    se modifico el proceso de guardado de imagen, para que no elimine la anterior si no se modifica 8

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -97,10 +97,7 @@ class Controller extends BaseController
     {
         foreach ($this->imageFields as $field) {
             $value = $request->input($field);
-            Log::info("Field '$field': type=" . gettype($value) .
-                ", hasFile=" . ($request->hasFile($field) ? 'true' : 'false') .
-                ", typeString=" . gettype($value) == "string" ? 'true' : 'false' .
-                ", filled=" . ($request->filled($field) ? 'true' : 'false'));
+            Log::info("Field '$field': type=" . gettype($value) . ", hasFile=" . ($request->hasFile($field) ? 'true' : 'false') . ", typeString=" . gettype($value) == "string" ? 'true' : 'false' . ", filled=" . ($request->filled($field) ? 'true' : 'false'));
 
             if ($request->hasFile($field)) {
                 $this->ImageUp(
@@ -115,7 +112,7 @@ class Controller extends BaseController
                 );
             } elseif (gettype($value) == "string") {
                 Log::info("entreeeee type=string: $value");
-                Log::info("campo: $model->$field");
+                Log::info("campo: $model[$field]");
                 // $model->$field = $value;
                 // $model->save();
             } elseif (!$request->filled($field)) {
