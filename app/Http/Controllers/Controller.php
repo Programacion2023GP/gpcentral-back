f07792290ea9bb48312017a54ba657b4f1486102
Nestor <nestorpuentesin@gmail.com>
2026-07-09 09:51:40 -0600

    se modifico el proceso de guardado de imagen, para que no elimine la anterior si no se modifica 6

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -97,7 +97,7 @@ class Controller extends BaseController
     {
         foreach ($this->imageFields as $field) {
             $value = $request->input($field);
-            Log::info("Field '$field': value=" . $value . " | type=" . gettype($value) .
+            Log::info("Field '$field': type=" . gettype($value) .
                 ", hasFile=" . ($request->hasFile($field) ? 'true' : 'false') .
                 ", typeString=" . gettype($value) == "string" ? 'true' : 'false' .
                 ", filled=" . ($request->filled($field) ? 'true' : 'false'));
@@ -114,7 +114,7 @@ class Controller extends BaseController
                     $model
                 );
             } elseif (gettype($value) == "string") {
-                Log::info("entreeeee type=string");
+                Log::info("entreeeee type=string: $value");
                 $model->$field = $value;
                 $model->save();
             } elseif (!$request->filled($field)) {
